c7f1db4a4a56603a94f3f5a779115b218aed42c2
danielson <dadjocy21@students.desu.edu>
2023-04-20 10:29:42 -0400

    added code

--- main/SMS_Spam.py
+++ main/SMS_Spam.py
@@ -25,3 +25,13 @@ X_train, X_test, y_train, y_test = train_test_split(df['sms_message'],
 print('Number of rows in the total set: {}'.format(df.shape[0]))
 print('Number of rows in the training set: {}'.format(X_train.shape[0]))
 print('Number of rows in the test set: {}'.format(X_test.shape[0]))
+
+from sklearn.feature_extraction.text import CountVectorizer
+# Instantiate the CountVectorizer method
+count_vector = CountVectorizer()
+
+# Fit the training data and then return the matrix
+training_data = count_vector.fit_transform(X_train)
+
+# Transform testing data and return the matrix. Note we are not fitting the testing data into the CountVectorizer()
+testing_data = count_vector.transform(X_test)
