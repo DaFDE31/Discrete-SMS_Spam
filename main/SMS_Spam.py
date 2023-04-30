d6eeb6c9838158506202c95249ec612e85311388
danielson <dadjocy21@students.desu.edu>
2023-04-30 19:30:48 -0400

    added code

--- main/SMS_Spam.py
+++ main/SMS_Spam.py
@@ -24,7 +24,7 @@ X_train, X_test, y_train, y_test = train_test_split(df['sms_message'],
 
 print('Number of rows in the total set: {}'.format(df.shape[0]))
 print('Number of rows in the training set: {}'.format(X_train.shape[0]))
-print('Number of rows in the test set: {}'.format(X_test.shape[0]))
+print('Number of rows in the test set: {}\n'.format(X_test.shape[0]))
 
 from sklearn.feature_extraction.text import CountVectorizer
 # Instantiate the CountVectorizer method
@@ -35,3 +35,15 @@ training_data = count_vector.fit_transform(X_train)
 
 # Transform testing data and return the matrix. Note we are not fitting the testing data into the CountVectorizer()
 testing_data = count_vector.transform(X_test)
+
+from sklearn.naive_bayes import MultinomialNB #BAYES
+naive_bayes = MultinomialNB()
+naive_bayes.fit(training_data, y_train)
+
+predictions = naive_bayes.predict(testing_data)
+
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+print('Accuracy score: ', format(accuracy_score(y_test, predictions)))
+print('Precision score: ', format(precision_score(y_test, predictions)))
+print('Recall score: ', format(recall_score(y_test, predictions)))
+print('F1 score: ', format(f1_score(y_test, predictions)))
